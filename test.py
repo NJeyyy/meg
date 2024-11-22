15ba98f0f43b8c20ba2bf31acc0495bf2e86db6c
boppy3279@gmail.com <boppy3279@gmail.com>
2024-11-22 07:24:53 +0700

    initiate the webpage, combining py and web files

--- test.py
+++ test.py
@@ -1,15 +1,30 @@
-from bottle import Bottle, run, response, request
-import json
+from bottle import response, request, template, static_file
+import bottle
 
-app = Bottle()
+itfp = bottle.app()
+class EnableCors(object):
+  name = 'enable_cors'
+  api = 2
+  def apply(self, fn, context):
+    def _enable_cors(*args, **kwargs):
+      # set CORS headers
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS, DELETE'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+      if bottle.request.method != 'OPTIONS':
+        # actual request; reply with the actual response
+        return fn(*args, **kwargs)
+    return _enable_cors
 
-@app.route('/trsrh', method='GET')
-def testsend():
-  response.content_type="application/json"
-  response.set_header("Access-Control-Allow-Origin", "*")
-  response.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-  response.set_header("Access-Control-Allow-Headers", "Content-Type")
-  return json.dumps({"a":75})
 
+@itfp.route('/<filepath:path>')
+def serve_static(filepath):
+    return static_file(filepath, root='./')
+@itfp.route("/")
+def loadp():
+  return template("itf.html")
+
+
+itfp.install(EnableCors())
 if __name__ == "__main__":
-  run(app, host='localhost', port=8080)
+  itfp.run(host='localhost', port = 8080)
